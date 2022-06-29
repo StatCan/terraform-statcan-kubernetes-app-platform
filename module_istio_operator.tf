4dfea88eb6bb306ddad810f5af6751b68c9b79e6
william.hearn <william.hearn@canada.ca>
2022-06-29 11:06:20 -0400

    feat(CN-1014): Istio to v1.8.6

--- module_istio_operator.tf
+++ module_istio_operator.tf
@@ -1,13 +1,13 @@
 # Deploys the Istio Operator controller along with the IstioOperator CRD.
 module "istio_operator" {
-  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-istio-operator.git?ref=v2.2.0"
+  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-istio-operator.git?ref=v2.4.0"
 
   depends_on = [
     module.namespace_istio_operator_system,
     module.namespace_istio_system,
   ]
 
-  tag = "1.7.8-distroless"
+  tag = "1.8.6-distroless"
 
   # The following are variables that can be specified, but come with sane defaults
   namespace        = module.namespace_istio_operator_system.name
---
+++ namespace_kiali_system.tf
@@ -0,0 +1,37 @@
+resource "kubernetes_namespace" "kiali_system" {
+  metadata {
+    name = "kiali-system"
+
+    labels = {
+      "namespace.statcan.gc.ca/purpose" = "system"
+    }
+  }
+}
+
+module "namespace_kiali_system" {
+  source = "git::https://github.com/canada-ca-terraform-modules/terraform-kubernetes-namespace.git?ref=v2.2.0"
+
+  name = kubernetes_namespace.kiali_system.id
+  namespace_admins = {
+    users  = []
+    groups = var.administrative_groups
+  }
+
+  # CI/CD
+  ci_name = var.ci_service_account_name
+
+  allowed_loadbalancers = tostring(1 * (length(var.additional_istio_ingress_gateways) + 1))
+  allowed_nodeports     = tostring(9 * (length(var.additional_istio_ingress_gateways) + 1))
+
+  # Image Pull Secret
+  enable_kubernetes_secret = var.platform_image_repository_credentials_enable
+  kubernetes_secret        = local.platform_image_pull_secret_name
+  docker_repo              = var.platform_image_repository
+  docker_username          = var.platform_image_repository_username
+  docker_password          = var.platform_image_repository_password
+  docker_email             = var.platform_image_repository_email
+  docker_auth              = var.platform_image_repository_auth
+
+  # Dependencies
+  dependencies = []
+}
